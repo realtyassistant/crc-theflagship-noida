57a52b3963219befb44203b0898b3f27336f7929
db21pawan <Pawan.Nagarkoti@thedigibee.in>
2025-04-29 15:23:29 +0530

    setup project and create components for all sec

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Header, HeroSec, ImgWithText, ProjectOverview, BrochureStrip, RowText, Pricing, Location, FooterConnect, Footer, FloorPlan } from "./components";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSec />
+      <ImgWithText />
+      <ProjectOverview />
+      <BrochureStrip />
+      <RowText />
+      <Pricing />
+      <BrochureStrip />
+      <FloorPlan />
+      <Location />
+      <FooterConnect />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
