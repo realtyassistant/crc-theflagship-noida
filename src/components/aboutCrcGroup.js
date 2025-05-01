c5a7cfdd11fd5b8de2454020589fd428f8f68ef0
db21pawan <Pawan.Nagarkoti@thedigibee.in>
2025-05-01 17:21:20 +0530

    added line below about us page

--- src/components/aboutCrcGroup.js
+++ src/components/aboutCrcGroup.js
@@ -5,6 +5,8 @@ export default function AboutCrcGroup() {
   return (
     <>
       <h1 className="custom-heading text-center text-themeColor">{aboutCrcGropupData?.heading}</h1>
+      <hr className="border-t-2 border-black mb-4" />
+
       <p className="text-justify">{aboutCrcGropupData?.paraOne}</p>
       <p className="text-justify">{aboutCrcGropupData?.paraTwo}</p>
       <p className="text-justify">{aboutCrcGropupData?.paraThree}</p>
