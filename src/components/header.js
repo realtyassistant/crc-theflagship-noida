53df87ddabbe384019cc6d598455546830dfb465
db21pawan <Pawan.Nagarkoti@thedigibee.in>
2025-05-02 14:11:43 +0530

    fixed header issue on mobile devic

--- src/components/header.js
+++ src/components/header.js
@@ -12,12 +12,12 @@ export default function Header() {
     <header className="bg-white text-black sticky top-0 left-0 w-full z-50 shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex">
-          <Image src="/logo.png" alt="logo" width={100} height={40} />
-          <Image src="/project-logo.webp" alt="logo" width={150} height={40} />
+          <Image src="/logo.png" alt="logo" width={100} height={40} className="w-32 h-auto" />
+          <Image src="/project-logo.webp" alt="logo" width={150} height={40} className="w-38 h-auto" />
         </div>
 
         {/* Desktop Navigation */}
-        <nav className="hidden md:flex gap-6">
+        <nav className="hidden lg:flex gap-6">
           {[
             { label: "Home", href: "/" },
             { label: "Overview", href: "#overview" },
@@ -34,7 +34,7 @@ export default function Header() {
         </nav>
 
         {/* Mobile Icon */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button onClick={toggleMenu} aria-label="Toggle menu">
             {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
@@ -45,7 +45,7 @@ export default function Header() {
       <div
         className={`fixed top-0 left-0 h-full w-64 bg-themeColor text-white transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-500 ease-in-out z-40 md:hidden`}
+        } transition-transform duration-500 ease-in-out z-40 lg:hidden`}
       >
         <div className="p-6 flex flex-col gap-6">
           {[
