0819933af2c41515ccf63217cc4fe655f6e79fec
db21pawan <Pawan.Nagarkoti@thedigibee.in>
2025-05-05 10:41:29 +0530

    fixed script issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./globals.css";
 import { Urbanist } from "next/font/google";
 import { phoneNumber } from "@/constant";
+import Script from "next/script";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -31,12 +32,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* <!-- Google Tag Manager --> */}
-        {/* <script>
-          {`  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        {/* Script from next/script is the only safe way to load inline scripts in Next.js apps. */}
+        {/* The id on the <Script> component is required to prevent duplication and improve debugging. */}
+        {/* Use strategy="afterInteractive" to load the tag after the page is interactive (per Google's recommendation). */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W2T5BPNP');`}
-        </script> */}
+        </Script>
       </head>
       {/* <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}> */}
       <body className={urbanist.variable}>
